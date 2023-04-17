d0e2bfbff1e1e2b87770d59cbce35ea35510443b
Ivanith <ivan.tkach.03@ukr.net>
2023-04-17 23:49:45 +0200

    Initial commit

---
+++ Instazoo/server/src/main/java/com/example/demo/entity/ImageModel.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class ImageModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] imageBytes;
+    @JsonIgnore
+    private Long userId;
+    @JsonIgnore
+    private Long postId;
+
+    public ImageModel() {
+    }
+}
